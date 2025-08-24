2b5d545158108f1f0b6ddd26c6725c53392dc79b
cme <info@cme-robotics.com>
2025-08-24 15:50:24 +0200

    Trying mirrored axis for front wheels.

--- launch/cmexa_mecanum.launch.py
+++ launch/cmexa_mecanum.launch.py
@@ -35,7 +35,7 @@ def generate_launch_description():
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
             PathJoinSubstitution(
-                [FindPackageShare("cmeresearch_description"), "urdf", "cmexa/cmexaiii.urdf.xacro"]
+                [FindPackageShare("cmeresearch_description"), "urdf", "cmexa/cmexa.urdf.xacro"]
             ),
             " ",
             "use_mock_hardware:=",
